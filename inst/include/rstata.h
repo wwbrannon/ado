9d841e7b2e8bf17504974b21a7038620e9590bef
William Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    Commands are now represented by a class rather than a typedef struct, and there's an ADD_TO_CMD_LIST macro

--- inst/include/rstata.h
+++ inst/include/rstata.h
@@ -44,51 +44,87 @@ typedef struct ast_node
 
 // We don't need to continue the parse tree all the way up to the level of a
 // translation unit because the command syntax is so constrained. Breaking it
-// out this way makes writing the R easier
-typedef struct STATA_CMD
+// out this way makes writing the R easier.
+class StataCmd
 {
-    // the command verb
-    char *verb;
-    
-    // "modifiers": a MODIFIER_LIST of the by, bysort, etc applied to the command
-    int has_modifiers; // 1 if there were modifiers applied, 0 otherwise
-    EXPR_T *modifiers;
+    public:
+        std::string n_verb; // the command verb
+        
+        EXPR_T *modifiers; // "modifiers": a MODIFIER_LIST of the by, bysort, etc, applied
+        EXPR_T *varlist; // a varlist is a left-deep tree of IDENTs
+        EXPR_T *assign_stmt; // "var = exp"
+        EXPR_T *if_exp; // "if expression"
+        EXPR_T *options; // ", options"
+        
+        int has_range;
+        int range_lower; // the lower range limit
+        int range_upper; // the upper range limit
+        
+        std::string *weight; // "weight": the column name of the weight, or NULL
+        std::string *using_filename; // "using filename": the filename given after using, or NULL
+        
+        StataCmd(std::string n_verb,
+                 std::string n_weight = "", std::string n_using_filename = "",
+                 int n_has_range = 0, int n_range_lower = 0, int n_range_upper = 0,
+                 EXPR_T *n_modifiers = NULL, EXPR_T *n_varlist = NULL,
+                 EXPR_T *n_assign_stmt = NULL, EXPR_T *n_if_exp = NULL,
+                 EXPR_T *n_options = NULL)
+        {
+            verb = n_verb;
 
-    // "varlist"
-    int has_varlist; // 1 if a varlist was provided, 0 otherwise
-    EXPR_T *varlist; // a varlist is a left-deep tree of IDENTs
-    
-    // "var = exp"
-    int has_assign; // 1 if an assignment expression was provided, 0 otherwise
-    EXPR_T *assign_stmt;
-    
-    // "if expression"
-    int has_if; // 1 if "if expression" was given, 0 otherwise
-    EXPR_T *if_exp;
-    
-    // "in range"
-    int has_range; // 1 if "in x/y" was given, 0 otherwise
-    int range_lower; // the lower range limit, or NULL
-    int range_upper; // the upper range limit, or NULL
-    
-    // "weight": the column name of the weight, or NULL
-    int has_weight;
-    char *weight;
-    
-    // "using filename": the filename given after using, or NULL
-    int has_using; // 1 if a using clause with filename was given, 0 otherwise
-    char *using_filename;
+            modifiers = n_modifiers;
+            varlist = n_varlist;
+            assign_stmt = n_assign_stmt;
+            if_exp = n_if_exp;
+            options = n_options;
 
-    // ", options"
-    int has_options; // 1 if any options were given, 0 otherwise
-    EXPR_T *options;
-} STATA_CMD_T;
+            has_range = n_has_range;
+            range_lower = n_range_lower;
+            range_upper = n_range_upper;
+
+            weight = n_weight;
+            using_filename = n_using_filename;
+        };
+};
 
 typedef struct STATA_CMD_LIST
 {
-    STATA_CMD_T *current;
-    STATA_CMD_T *next;
+    StataCmd *current;
+    StataCmd *next;
 } STATA_CMD_LIST_T;
+
+// the initial empty command list the update macro will work with,
+// and the head pointer to the list
+STATA_CMD_LIST_T cmdlist =
+{
+    .current = NULL,
+    .next = NULL
+};
+
+STATA_CMD_LIST_T *cur = &cmdlist;
+STATA_CMD_LIST_T *head = &cmdlist;
+
+#ifndef ADD_TO_CMD_LIST
+#define ADD_TO_CMD_LIST(cmd, cmdlist_ptr)           \
+{                                                   \
+        if(cur->current == NULL)                    \
+        {                                           \
+            cur->current = &cmd;                    \
+        }                                           \
+        else                                        \
+        {                                           \
+            STATA_CMD_LIST_T next_cmdlist =         \
+            {                                       \
+                .current = &cmd;                    \
+                .next = NULL                        \
+            };                                      \
+                                                    \
+            cur->next = &next_cmdlist;              \
+            cur = &next_cmdlist;                    \
+        }                                           \
+        cmdlist_ptr = head;                         \
+}
+#endif /* ADD_TO_CMD_LIST */
 
 #endif /* __RSTATA_H__ */
 
