0ea262f3c4373fca26ad83a94fdd7ebce589b4f2
Will Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    the callback parser shouldn't return the AST

--- src/do_stata_parse.cpp
+++ src/do_stata_parse.cpp
@@ -7,11 +7,10 @@
 // [[Rcpp::plugins("cpp11")]]
 
 // [[Rcpp::export]]
-Rcpp::List
+SEXP
 do_parse_with_callbacks(std::string text, Rcpp::Function cmd_action,
                         Rcpp::Function get_macro_value, int debug_level=0)
 {
-    Rcpp::List res;
     RStataDriver *driver = new RStataDriver(1, cmd_action, get_macro_value,
                                             text, debug_level);
 
@@ -19,11 +18,10 @@ do_parse_with_callbacks(std::string text, Rcpp::Function cmd_action,
     if( driver->parse() != 0 || driver->error_seen != 0)
         return R_NilValue;
 
-    // now take the resulting AST and recursively turn it into an R object
-    res = driver->ast->as_R_object();
     driver->delete_ast();
+    delete driver;
 
-    return res;
+    return R_NilValue;
 }
 
 // [[Rcpp::export]]
@@ -39,7 +37,9 @@ do_parse(std::string text, int debug_level=0)
 
     // now take the resulting AST and recursively turn it into an R object
     res = driver->ast->as_R_object();
+    
     driver->delete_ast();
+    delete driver;
 
     return res;
 }
