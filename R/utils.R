d4b8b44ac6e1236316f269e669cd3a96787bdd1d
Will Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    renaming some files and creating a raiseifnot function for assertions in semantic analysis

--- R/utils.R
+++ R/utils.R
@@ -8,6 +8,44 @@ function(str)
     simplify2array(pts)
 }
 
+raiseCondition <-
+function(cls, msg)
+{
+    cond <- simpleCondition(msg)
+    class(cond) <- c(class(cond), cls)
+    signalCondition(cond)
+
+    invisible(NULL)
+}
+
+raiseifnot <-
+function(expr, cls="bad_command", msg=errmsg)
+{
+    #Construct a message
+    mc <- match.call()
+    ch <- deparse(mc[[1]], width.cutoff = 60L)
+    if (length(ch) > 1L) 
+        ch <- paste(ch[1L], "....")
+    errmsg <- sprintf("%s is not TRUE", ch)
+    
+    #Check and raise a condition if it fails
+    if (!(is.logical(expr) && !is.na(expr) && expr))
+        raiseCondition(cls, msg)
+    
+    invisible(NULL)
+}
+
+
+stopifnot
+function (...) 
+{
+    mc <- match.call()
+    if (!(is.logical(r <- ll[[i]]) && !any(is.na(r)) && 
+        all(r))) {
+    }
+    invisible()
+}
+
 #We're reading from the console, so we have to handle the /// construct
 #in this function as well as in the parser.
 read_interactive <-
@@ -19,11 +57,7 @@ function()
         inpt <- readline(". ")
         
         if(length(inpt) == 0) #at EOF
-        {
-            cond = simpleCondition("end of file")
-            class(cond) <- c("exit", class(cond))
-            signalCondition(cond)
-        }
+            raiseCondition("exit", "end of file")
         
         if(substring(rev_string(inpt), 1, 3) == "///")
         {
