8a95aca91444db405f3d378b1abcc9573ec8ffdd
William Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    some incomplete steps to reorganize the C++ code - everything should be under one hierarchy of AST nodes

--- inst/include/rstata.hpp
+++ inst/include/rstata.hpp
@@ -1,9 +1,9 @@
-// Stata is a very unusual language, more like the Bourne shell than it is like C.
-// A Stata script consists of one or more "commands" in sequence. Each command starts with
-// a verb and then has various kinds of arguments and options after it.
-// Syntax is not fully linear, so one command is one line. (But there's a /// syntax
-// to continue a command across lines, and certain flow-control constructs like forval span
-// more than one line as well.)
+// Stata is a very unusual language. It resembles the Unix shell(s) than more traditional
+// programming languages. A Stata script is one or more commands, which are possibly compound
+// statement blocks or loops with an associated block, executed in the order they appear in the file.
+// Each non-compound command starts with a verb and then has various kinds of arguments and options
+// after it. In general, one non-compound command is one line, but there is a /// syntax to continue
+// a command across a line break.
 
 // The basic Stata command syntax is:
 // [ (prefix [arguments])+:] command [varlist | var = exp] [if expression] [in range] [weight] [using filename] [, options]
@@ -14,12 +14,58 @@
 #include <memory>
 #include <Rcpp.h>
 
-// Options as they occur after commands, prefix or otherwise
-class StataOption
+// abstract base class for the AST the parser will generate
+class BaseExprNode
 {
     public:
-        StataOption(std::string _name, std::vector<std::string> _args);
-        Rcpp::List as_list() const;
+        // method that returns this BaseExprNode as an R expression
+        virtual Rcpp::Language as_R_object() const = 0;
+};
+
+// The next three classes are AST nodes for literals - string, symbol and numeric
+class NumberExprNode: public BaseExprNode
+{
+    public:
+        NumberExprNode(signed long int _data);
+        NumberExprNode(unsigned long int _data);
+        NumberExprNode(long double _data);
+        NumberExprNode(std::string _data);
+        
+        virtual Rcpp::Language as_R_object() const;
+
+    private:
+        Rcpp::NumericVector data; // a type-generic number already in R format
+};
+
+class IdentExprNode: public BaseExprNode
+{
+    public:
+        IdentExprNode(std::string _data);
+        
+        virtual Rcpp::Language as_R_object() const;
+
+    private:
+        std::string data;
+};
+
+class StringExprNode: public BaseExprNode
+{
+    public:
+        StringExprNode(std::string _data);
+        
+        virtual Rcpp::Language as_R_object() const;
+
+    private:
+        std::string data;
+};
+
+// Options as they occur after commands, prefix or otherwise
+class OptionExprNode: public BaseExprNode
+{
+    public:
+        OptionExprNode(std::string _name, std::vector<std::string> _args);
+        
+        virtual Rcpp::List as_R_object() const;
 
     private:
         std::string name;
@@ -27,106 +73,52 @@ class StataOption
 };
 
 // Option lists
-class OptionList
+class OptionListExprNode: public BaseExprNode
 {
     public:
-        OptionList(std::vector<StataOption> _options);
-        Rcpp::List as_list() const;
+        OptionListExprNode(std::vector<OptionExprNode> _options);
+        virtual Rcpp::List as_R_object() const;
     
     private:
-        std::vector<StataOption> options;
-};
-
-// abstract base class for Stata expressions
-class BaseStataExpr
-{
-    public:
-        // method that returns this BaseStataExpr as an R expression
-        virtual Rcpp::Language as_expr() const = 0;
-};
-
-class NumberStataExpr: public BaseStataExpr
-{
-    public:
-        NumberStataExpr(signed long int _data);
-        NumberStataExpr(unsigned long int _data);
-        NumberStataExpr(long double _data);
-        
-        virtual Rcpp::Language as_expr() const;
-
-    private:
-        Rcpp::NumericVector data; // a type-generic number already in R format
-};
-
-class IdentStataExpr: public BaseStataExpr
-{
-    public:
-        IdentStataExpr(std::string _data);
-        
-        virtual Rcpp::Language as_expr() const;
-
-    private:
-        std::string data;
-};
-
-class StringStataExpr: public BaseStataExpr
-{
-    public:
-        StringStataExpr(std::string _data);
-        
-        virtual Rcpp::Language as_expr() const;
-
-    private:
-        std::string data;
+        std::vector<OptionExprNode> options;
 };
 
 // all other expressions: assignment expressions, logical expressions,
 // equality expressions, relational expressions, arithmetic expressions,
-// unary expressions and function calls
-class BranchStataExpr: public BaseStataExpr
+// unary expressions and 'postfix' expressions like function calls
+class BranchExprNode: public BaseExprNode
 {
     public:
-        BranchStataExpr(std::string _data, std::vector<std::unique_ptr<BaseStataExpr>> _children);
+        BranchExprNode(std::string _data, std::vector<std::unique_ptr<BaseExprNode>> _children);
         
-        virtual Rcpp::Language as_expr() const;
+        virtual Rcpp::Language as_R_object() const;
     
     private:
-        std::vector<std::unique_ptr<BaseStataExpr>> children;
+        std::vector<std::unique_ptr<BaseExprNode>> children;
         std::string data;
 };
 
-// We don't need to continue the parse tree all the way up to the level of a
-// translation unit because the command syntax is so constrained. Breaking it
-// out into BaseStataCmd and its derived classes at the top level makes this
-// somewhat easier.
-
-// the abstract base class for Stata commands
-class BaseStataCmd
-{
-    public:
-        std::string verb; // the command verb
-        
-        // method that returns this StataCmd as an R list
-        virtual Rcpp::List as_list() const = 0;
-};
-
-class EmbeddedRCmd: public BaseStataCmd
+// The "embedded R" block that the lexer recognizes and passes through
+class EmbeddedRCmd: public BaseExprNode
 {
     private:
         std::string text;
 
     public:
-        virtual Rcpp::List as_list() const;
+        std::string verb;
+        
+        virtual Rcpp::List as_R_object() const;
         EmbeddedRCmd(std::string _text);
 };
 
-class GeneralStataCmd: public BaseStataCmd
+// All non-compound Stata commands
+class GeneralStataCmd: public BaseExprNode
 {
     private:
-        BaseStataExpr *varlist;
-        BaseStataExpr *assign_stmt; // "var = exp"
-        BaseStataExpr *if_exp; // "if expression"
-        OptionList    *options; // ", options"
+        BaseExprNode *varlist;
+        BaseExprNode *assign_stmt; // "var = exp"
+        BaseExprNode *if_exp; // "if expression"
+        OptionListExprNode    *options; // ", options"
         
         int has_range;
         int range_lower; // the lower range limit
@@ -136,17 +128,63 @@ class GeneralStataCmd: public BaseStataCmd
         std::string using_filename; // "using filename": the filename given after using, or NULL
     
     public:
-        BaseStataCmd *ChildCmd;
-        virtual Rcpp::List as_list() const;
+        std::string verb;
+
+        BaseExprNode *ChildCmd; // the command after a prefix command, incl another prefix command
+        virtual Rcpp::List as_R_object() const;
 
         GeneralStataCmd(std::string _verb,
                    std::string _weight, std::string _using_filename,
                    int _has_range, int _range_lower, int _range_upper,
-                   BaseStataExpr *_varlist, BaseStataExpr *_assign_stmt,
-                   BaseStataExpr *_if_exp, OptionList *_options);
+                   BaseExprNode *_varlist, BaseExprNode *_assign_stmt,
+                   BaseExprNode *_if_exp, OptionListExprNode *_options);
 };
 
-// positional-only parameters are garbage...
+class CmdBlock: public BaseExprNode
+{
+    private:
+        
+
+    public:
+        virtual Rcpp::List as_R_object() const;
+};
+
+class WhileCmd: public BaseExprNode
+{
+    private:
+        CmdBlock *cmds;
+        std::string name;
+        BaseExprNode *exp;
+
+    public:
+        virtual Rcpp::List as_R_object() const;
+        WhileCmd(CmdBlock *cmds
+};
+
+class ForvaluesCmd: public BaseExprNode
+{
+    private:
+        CmdBlock *cmds;
+        std::string name;
+
+
+    public:
+        virtual Rcpp::List as_R_object() const;
+};
+
+class ForeachCmd: public BaseExprNode
+{
+    private:
+        CmdBlock *cmds;
+        std::string name;
+
+
+    public:
+        virtual Rcpp::List as_R_object() const;
+};
+
+// A helper class to avoid typing out all the args to the GeneralStataCmd constructor.
+// Positional-only parameters are garbage...
 class MakeGeneralStataCmd
 {
     public:    
@@ -155,10 +193,10 @@ class MakeGeneralStataCmd
         GeneralStataCmd create();
 
         MakeGeneralStataCmd& verb(std::string const& _verb);
-        MakeGeneralStataCmd& varlist(BaseStataExpr *_varlist);
-        MakeGeneralStataCmd& assign_stmt(BaseStataExpr *_assign_stmt);
-        MakeGeneralStataCmd& if_exp(BaseStataExpr *_if_exp);
-        MakeGeneralStataCmd& options(OptionList *_options);
+        MakeGeneralStataCmd& varlist(BaseExprNode *_varlist);
+        MakeGeneralStataCmd& assign_stmt(BaseExprNode *_assign_stmt);
+        MakeGeneralStataCmd& if_exp(BaseExprNode *_if_exp);
+        MakeGeneralStataCmd& options(OptionListExprNode *_options);
         MakeGeneralStataCmd& has_range(int _has_range);
         MakeGeneralStataCmd& range_upper(int _range_upper);
         MakeGeneralStataCmd& range_lower(int _range_lower);
@@ -167,10 +205,10 @@ class MakeGeneralStataCmd
     
     private:
         std::string __verb;
-        BaseStataExpr *__varlist;
-        BaseStataExpr *__assign_stmt;
-        BaseStataExpr *__if_exp;
-        OptionList *__options;
+        BaseExprNode *__varlist;
+        BaseExprNode *__assign_stmt;
+        BaseExprNode *__if_exp;
+        OptionListExprNode *__options;
         int __has_range;
         int __range_lower;
         int __range_upper;
