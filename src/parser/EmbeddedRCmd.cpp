4b7b4d8f2023d79a8a5a992bf9ce21974fb2b720
William Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    restoring mistakenly deleted rstata.hpp and adding an EmbeddedRCmd class for embedded R code

---
+++ src/parser/EmbeddedRCmd.cpp
@@ -0,0 +1,22 @@
+#include <Rcpp.h>
+#include "rstata.hpp"
+
+using namespace Rcpp;
+
+EmbeddedRCmd::EmbeddedRCmd(std::string _text)
+{
+    verb = "embedded_r";
+    text = _text;
+}
+
+List EmbeddedRCmd::as_list() const
+{
+    List res;
+   
+    res = List::create(_["func"]            = Symbol("dispatch.rstata.cmd"),
+                       _["verb"]            = verb,
+                       _["text"]            = text,
+    
+    return res;
+}
+
