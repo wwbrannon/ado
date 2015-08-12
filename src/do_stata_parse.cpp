573872b60648b014bba8f01c2a9acf420ca805f7
William Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    fixing the do_stata_parse function

--- src/do_stata_parse.cpp
+++ src/do_stata_parse.cpp
@@ -9,26 +9,35 @@ using namespace Rcpp;
 List do_stata_parse(std::string line)
 {
     YY_BUFFER_STATE   buf;
-    STATA_CMD_LIST_T  cmdlist;
-    StataCmd          *cur, *next;
+    StataCmd          *obj;
     List              ret;
+    
+    STATA_CMD_LIST_T  start;
+    STATA_CMD_LIST_T  *parsed;
+
+    parsed = &start;
 
     // handle some buffers and parse the input
     buf = yy_scan_string(line.c_str());
-    if( yyparse(&cmdlist) != 0 )
+    if( yyparse(parsed) != 0 )
         return R_NilValue;
     yy_delete_buffer(buf);
 
     // now take the resulting STATA_CMD_LIST_T and turn it into an R call object
-    do {
+    while(true)
+    {
+        obj = parsed->current;
+
         // ask the StataCmd object to give us its R form
-        List res = (*cur).as_list();
+        List res = (*obj).as_list();
         
         ret = Language("list", ret, res).eval(); // append res to ret
         
-        if(next)
-            cmdlist = cmdlist->next;
-    } while(next);
+        if(parsed->next != NULL)
+            parsed = parsed->next;
+        else
+            break;
+    }
 
     return ret;
 }
