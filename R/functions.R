5382b5a4ad500737892807db836db79b182a0558
Will Brannon <will.brannon@gmail.com>
2015-08-12 00:00:00 +0000

    actions for collapse, and more bugfixes in new actions

--- R/functions.R
+++ R/functions.R
@@ -35,3 +35,39 @@ function(vec)
     #FIXME
 }
 
+rstata_func_recode_rule_ident <-
+function(args)
+{
+    #FIXME
+}
+
+rstata_func_recode_rule_range <-
+function(args)
+{
+    #FIXME
+}
+
+rstata_func_recode_rule_numlist <-
+function(args)
+{
+    #FIXME
+}
+
+rstata_func_collapse_stata <-
+function(args)
+{
+    #FIXME
+}
+
+rstata_func_collapse_reassign <-
+function(args)
+{
+    #FIXME
+}
+
+rstata_func_collapse_newvar <-
+function(args)
+{
+    #FIXME
+}
+
