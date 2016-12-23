14c0f444f5a6122c503f66d7b42d2901198069d4
Will Brannon <will.brannon@gmail.com>
2016-12-23 10:43:03 +0000

    More travis updates

--- travis-install.sh
+++ travis-install.sh
@@ -28,4 +28,4 @@ cat /etc/apt/sources.list | sed -e 's/precise/trusty/g' > /tmp/trusty.list
 sudo mv /tmp/trusty.list /etc/apt/sources.list.d/trusty.list
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y upgrade
