979be501ae954b1a493caafc872b2a2f766d38e5
Will Brannon <will.brannon@gmail.com>
2016-12-23 10:30:21 +0000

    Try to get travis to install dependencies

---
+++ travis-install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Install more recent versions of certain packages than
+# travis's R support includes by default
+
+sudo cat > /etc/apt/preferences << EOF
+Package: bison
+Pin: release n=trusty
+Pin-Priority: 990
+
+Package: libbison-dev
+Pin: release n=trusty
+Pin-Priority: 990
+
+Explanation: Uninstall or do not install any Ubuntu-originated
+Explanation: package versions other than those in precise
+Package: *
+Pin: release n=precise
+Pin-Priority: 900
+
+Package: *
+Pin: release o=Ubuntu
+Pin-Priority: -10
+EOF
+
+sudo cat /etc/apt/sources.list | sed 's/precise/trusty' > /etc/apt/sources.list.d/trusty.list
+
+sudo apt-get update
+sudo apt-get upgrade
