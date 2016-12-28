abc58a128ba12aa8cdb6694b0645b86585302bbb
Will Brannon <will.brannon@gmail.com>
2016-12-28 23:55:41 +0000

    Make logging instantiation part of initialize/finalize

--- R/zzz.R
+++ R/zzz.R
@@ -31,26 +31,30 @@ function()
 {
     #We want it in a known-good state before initializing
     finalize()
-    
+
+    #===================================================
+    #Set up the logging handler object
+    assign("ado_logger", Logger$new(), envir=ado_env)
+
     #===================================================
     #Set up the dataset object
     assign("ado_dta", Dataset$new(), envir=ado_env)
-    
+
     #===================================================
     #Create environments to represent Stata's "e-class" and "r-class" objects
     #for stored results
     assign("ado_rclass_env", new.env(parent=emptyenv()), envir=ado_env)
     assign("ado_eclass_env", new.env(parent=emptyenv()), envir=ado_env)
-    
+
     #===================================================
     #Another env for c-class objects that's not the only place c-class values
     #are looked up. Still need to set certain values here.
     cc_env <- new.env(parent=emptyenv())
-    
+
     #Mathematical constants
     assign("pi", pi, envir=cc_env)
     assign("e", exp(1), envir=cc_env)
-    
+
     #Letters
     assign("alpha", paste0(letters, collapse=" "), envir=cc_env)
     assign("ALPHA", paste0(LETTERS, collapse=" "), envir=cc_env)
@@ -64,26 +68,26 @@ function()
     #Weekdays
     Wdays <- weekdays(seq(as.Date("2013-06-03"), by=1, len=7), abbreviate=TRUE)
     Wdays <- paste0(Wdays, collapse=" ")
-    
+
     Weekdays <- weekdays(seq(as.Date("2013-06-03"), by=1, len=7))
     Weekdays <- paste0(Weekdays, collapse=" ")
-    
+
     assign("Wdays", Wdays, envir=cc_env)
     assign("Weekdays", Weekdays, envir=cc_env)
 
     #URLs for webuse
     assign('default_webuse_url', 'http://www.stata-press.com/data/r13/', envir=cc_env)
-    
+
     assign("ado_cclass_env", cc_env, envir=ado_env)
-    
+
     #===================================================
     #Create the settings cache and macro symbol table
     assign("ado_macro_env", new.env(parent=emptyenv()), envir=ado_env)
-    
+
     s_env <- new.env(parent=emptyenv())
     assign("webuse_url", 'http://www.stata-press.com/data/r13/', envir=s_env)
     assign("ado_settings_env", s_env, envir=ado_env)
-    
+
     return(invisible(NULL))
 }
 
@@ -100,8 +104,15 @@ function()
         dt <- get("ado_dta", envir=ado_env)
         dt$clear()
     }
-    
+
+    #Turn off all logging if any sinks are still open
+    if("ado_logger" %in% ls(envir=ado_env))
+    {
+        lg <- get("ado_logger", envir=ado_env)
+        lg$deregister_all_sinks()
+    }
+
     rm(list=ls(envir=ado_env), envir=ado_env)
-    
+
     return(invisible(NULL))
 }
