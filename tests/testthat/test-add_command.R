d86c7c14ba7e96da7708761a4d76bba64c71da19
Will Brannon <will.brannon@gmail.com>
2016-12-28 07:09:11 +0000

    updated test for addCommand

--- tests/testthat/test-add_command.R
+++ tests/testthat/test-add_command.R
@@ -4,12 +4,33 @@ test_that("Adding user commands works", {
     nsenv <- getNamespace('ado')
     pkgenv <- as.environment('package:ado')
 
-    cmd <- function(expression) { return(as.character(expression)) }
-
+    #Make sure that our target name is not already loaded
     expect_false('testprint' %in% ls(envir=nsenv))
 
-    ado(string='addCommand cmd, newname(testprint)')
+    #The function we're turning into a command
+    cmd <- function(expression) { return(as.character(expression)) }
 
-    expect_true('ado_cmd_testprint' %in% ls(envir=nsenv &&
-                'ado_cmd_testprint' %not_in% ls(envir=pkgenv)))
+    #Hack alert! We have to put this into the globalenv to load it.
+    tn <- temporary_name(lst=ls(envir=globalenv()))
+
+    tryCatch(
+        {
+            assign(tn, cmd, envir=globalenv())
+
+            #Actually load the thing - this is what we're testing here
+            st <- 'addCommand ' %p% tn %p% ', newname(testprint)'
+            ado(string=st)
+        }, finally=
+        {
+            #Tear down - remove this function from the globalenv
+            if(tn %in% ls(envir=globalenv()))
+                rm(list=tn, envir=globalenv())
+        })
+
+    #Check that it worked
+    expect_true('ado_cmd_testprint' %in% ls(envir=nsenv) &&
+                'ado_cmd_testprint' %not_in% ls(envir=pkgenv))
+
+    if('ado_cmd_testprint' %in% ls(envir=nsenv))
+        expect_equal(get('ado_cmd_testprint', envir=nsenv), cmd) 
 })
