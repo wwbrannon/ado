2364bbfe817e3bcdb9619202443eca796412ad94
William Brannon <will.brannon@gmail.com>
2018-07-29 18:53:26 -0400

    fix cleanup script

--- tools/config/cleanup.R
+++ tools/config/cleanup.R
@@ -1,12 +1,21 @@
 # Clean up files generated during configuration here.
 # Use 'remove_file()' to remove files generated during configuration.
 
+remove_pattern <-
+function(path, pattern)
+{
+    lst <- list.files(path=path, pattern=pattern, full.names=TRUE,
+                      recursive=TRUE)
+
+    sapply(lst, remove_file)
+}
+
 remove_file("src/Makevars")
 remove_file("src/Makevars.win")
 remove_file("src/parser/build")
 
-sapply(list.files(path='src/', pattern='\\.o$', recursive=TRUE), remove_file)
-sapply(list.files(path='src/', pattern='\\.a$', recursive=TRUE), remove_file)
-sapply(list.files(path='src/', pattern='\\.so$', recursive=TRUE), remove_file)
-sapply(list.files(path='src/', pattern='\\~$', recursive=TRUE), remove_file)
+remove_pattern(path='src', pattern='\\.o$')
+remove_pattern(path='src', pattern='\\.a$')
+remove_pattern(path='src', pattern='\\.so$')
+remove_pattern(path='src', pattern='\\~$')
 
